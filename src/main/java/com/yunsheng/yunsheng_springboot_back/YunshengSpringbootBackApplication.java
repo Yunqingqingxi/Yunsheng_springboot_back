24363f5b57945eb2740747a71035531f1ecdd504
Niko <y111626395@outlook.com>
2024-06-25 23:38:31 +0800

    创建项目
    Signed-off-by: Niko <y111626395@outlook.com>

---
+++ src/main/java/com/yunsheng/yunsheng_springboot_back/YunshengSpringbootBackApplication.java
@@ -0,0 +1,13 @@
+package com.yunsheng.yunsheng_springboot_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YunshengSpringbootBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YunshengSpringbootBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yunsheng/yunsheng_springboot_back/YunshengSpringbootBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yunsheng.yunsheng_springboot_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YunshengSpringbootBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
