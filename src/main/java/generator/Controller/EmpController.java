8d13553fbcdfc4d098b0e880f48bb1c93c8ee29e
Niko <y111626395@outlook.com>
2024-06-26 23:25:49 +0800

    修正配置
    Signed-off-by: Niko <y111626395@outlook.com>

--- src/main/java/generator/Controller/EmpController.java
+++ src/main/java/generator/Controller/EmpController.java
@@ -5,7 +5,6 @@ import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import generator.Common.PageCode;
 import generator.Common.R;
 import generator.Exception.UserExe;
-import generator.domain.Dto.EmpDto;
 import generator.domain.Employee;
 import generator.domain.VO.EmpVo;
 import generator.service.EmployeeService;
@@ -13,11 +12,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Date;
 import java.util.List;
 
 import static generator.util.Jwt.createToken;
-
 
 @RestController
 @Slf4j
@@ -37,6 +34,7 @@ public class EmpController {
         if (emp != null) {
             String token = createToken(Long.valueOf(emp.getId()));
             EmpVo empVO = new EmpVo(emp.getId(), emp.getName(), token, emp.getUsername());
+
             return R.success(empVO);
         }
         return R.error("账号或密码错误");
@@ -44,7 +42,7 @@ public class EmpController {
 
     @GetMapping("/page")
     public R<PageCode> page(int page, int pageSize, String name) {
-        Page<Employee> info = new Page<>(page, pageSize);
+        Page<Employee> info = new Page<>();
         LambdaQueryWrapper<Employee> lambdaQueryWrapper = new LambdaQueryWrapper<>();
         lambdaQueryWrapper.like(name != null, Employee::getName, name);
         lambdaQueryWrapper.orderByDesc(Employee::getUpdateTime);
@@ -57,53 +55,9 @@ public class EmpController {
     }
 
     @PutMapping("/editPassword")
-    public R<String> editPassword(@RequestBody EmpDto empDto) {
-        if (empDto.getOldPassword() == empDto.getNewPassword())
-            throw new UserExe("新密码不能与旧密码相同");
-        else {
-            Employee byId = employeeService.getById(empDto.getEmpId());
-            byId.setPassword(empDto.getNewPassword());
-        }
+    public R<String> editPassword(@RequestBody Employee employee) {
+
         return R.success("修改密码成功");
-    }
-
-    @PostMapping("/status/{status}")
-    public R<String> status(@PathVariable int status, Long id) {
-        Employee employee = employeeService.getById(id);
-        employee.setStatus(status);
-        employeeService.updateById(employee);
-        return R.success("修改状态成功");
-    }
-
-    @PostMapping()
-    public R<String> add(@RequestBody Employee employee) {
-        employee.setPassword("123456");
-        employee.setCreateTime(new Date());
-        employee.setUpdateTime(new Date());
-        employee.setName("员工");
-        employee.setIdNumber("14888791541");
-        // 默认是1 启用状态
-        employee.setStatus(1);
-
-        employeeService.save(employee);
-        return R.success("添加成功");
-    }
-
-    @GetMapping("/{id}")
-    public R<Employee> getById(@PathVariable Long id) {
-        Employee employee = employeeService.getById(id);
-        return R.success(employee);
-    }
-
-    @PutMapping()
-    public R<String> edit(@RequestBody Employee employee) {
-        employeeService.updateById(employee);
-        return R.success("修改成功");
-    }
-
-    @PostMapping("/logout")
-    public R<String> logout() {
-        return R.success("退出成功");
     }
 
 
@@ -111,23 +65,25 @@ public class EmpController {
     public R<String> register(@RequestBody Employee employee) {
         LambdaQueryWrapper<Employee> lambdaQueryWrapper = new LambdaQueryWrapper<>();
         lambdaQueryWrapper.eq(Employee::getUsername, employee.getUsername());
-        Employee employee1 = employeeService.getOne(lambdaQueryWrapper);
-        if (employee1 == null) {
-            Employee employee2 = new Employee();
-            employee2.setPassword("123456");
-            employee2.setName("管理员");
-            employee2.setStatus(1);
-            employee2.setUsername(employee.getUsername());
-            employee2.setUpdateTime(new Date());
-
-            employee2.setCreateTime(new Date());
-            employee2.setUpdateTime(new Date());
-            employeeService.save(employee2);
-            return R.success("注册成功");
-
-        } else {
+        Employee user1 = employeeService.getOne(lambdaQueryWrapper);
+        if (user1 != null) {
             throw new UserExe("用户名已存在");
         }
+
+        employeeService.save(employee);
+        return R.success("注册成功");
     }
 
+    @DeleteMapping("/{name}")
+    public R<String> delete(@PathVariable String name) {
+        LambdaQueryWrapper<Employee> lambdaQueryWrapper = new LambdaQueryWrapper<>();
+        lambdaQueryWrapper.eq(Employee::getUsername, name);
+        Employee employee = employeeService.getOne(lambdaQueryWrapper);
+        if (employee != null) {
+            employeeService.removeById(employee.getId());
+            return R.success("删除成功");
+        } else {
+            return R.error(null);
+        }
+    }
 }
